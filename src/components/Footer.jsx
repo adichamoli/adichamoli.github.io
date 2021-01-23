3e2f85ffa81e3c6dbe70a2f50163c167ed62af4d
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-01-23 22:17:54 +0530

    Aditya Portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,10 +12,10 @@ const Footer = (props) => {
         <a
           className="badge badge-dark"
           rel="noopener"
-          href="https://github.com/hashirshoaeb"
+          href="https://github.com/adichamoli"
           aria-label="My GitHub"
         >
-          Hashir Shoaib
+          Aditya Chamoli
         </a>{" "}
         using <i className="fab fa-react" />
         <p>
--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -37,10 +37,17 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: require("../editable-stuff/profile.jpg"),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
+    "My name is Aditya Chamoli. I am working as Senior Software Developer in Amdocs India. \
+	My work mainly involves Development in C, C#, Unix, Sql, RestFul Web Services. \
+	I have deep inclination towards Data Science and Machine Learning. In my free time, I like to \
+	learn about new tools and technology. I'm most passionate about giving back to the community, \
+	and my goal is to pursue this passion within the field of software engineering. \
+	I believe education should be free and reach to everyone, to support i keep on updating \
+	my Github Repos and encourage to learn, fork or update to it.\
+	Also One should never stop learning.",
   resume: require("../editable-stuff/resume.pdf"),
 };
 
@@ -48,9 +55,9 @@ const about = {
 const repos = {
   show: true,
   heading: "Recent Projects",
-  gitHubUsername: "adichamoli", //i.e."johnDoe12Gh"
-  reposLength: 12,
-  specificRepos: [],
+  gitHubUsername: "adichamoli",
+  reposLength: 0,
+  specificRepos: ["Data-Structures", "gRPC_Python", "MachineLearning", "DataScience", "SpringBootAngularMongo", "DeepLearning", "ML-Data-Science-Projects", "ProjectEulerHackerrank", "HackerRankPython", "Database"],
 };
 
 // Leadership SECTION
@@ -61,12 +68,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
+      img: require("../editable-stuff/profile.jpg"), 
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
+      img: require("../editable-stuff/profile.jpg"), 
       label: "Second slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
@@ -82,24 +89,30 @@ const skills = {
   show: true,
   heading: "Skills",
   hardSkills: [
+    { name: "C/C++", value: 80 },
+	{ name: "C#", value: 65 },
     { name: "Python", value: 90 },
+	{ name: "Java", value: 60 },
     { name: "SQL", value: 75 },
+	{ name: "PowerShell", value: 75 },
     { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+	{ name: "Pro*C", value: 70 },
+    { name: "Configuration Management", value: 80 },
+	{ name: "Data Science", value: 60 },
+	{ name: "Machine Learning", value: 70 },
+	{ name: "Web Services", value: 70 },
   ],
   softSkills: [
     { name: "Goal-Oriented", value: 80 },
     { name: "Collaboration", value: 90 },
-    { name: "Positivity", value: 75 },
+    { name: "Positivity", value: 95 },
     { name: "Adaptability", value: 85 },
     { name: "Problem Solving", value: 75 },
     { name: "Empathy", value: 90 },
     { name: "Organization", value: 70 },
     { name: "Creativity", value: 90 },
+	{ name: "Team Work", value: 90 },
+	{ name: "Versatility", value: 90 },
   ],
 };
 
@@ -108,13 +121,8 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    "I'm currently looking for full-time Software Engineering, Data Science and Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "adichamoli@gmail.com",
 };
-
-// Blog SECTION
-// const blog = {
-//   show: false,
-// };
 
 export { navBar, mainBody, about, repos, skills, leadership, getInTouch };
